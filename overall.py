a1f56776f7df0597a0649e14050d0072ce434a8d
sofiarosan <anarodsan@unisabana.edu.co>
2026-04-05 11:36:37 -0500

    promemdios 1

---
+++ overall.py
@@ -0,0 +1,22 @@
+def student_averages(data):
+    result={}
+
+    for student, grades in data.items():
+        avg= sum(grades.values())/len(grades)
+        result[student]=round(avg)
+    return result
+
+def assigment_averages(data):
+    result={}
+
+    for student in data:
+        assigments = data[student].keys()
+    for assigment in assigments:
+        total=0
+        count=0
+        for student in data:
+           total += data[student][assigment]
+           count += 1
+        result[assigment] =round(total/count)
+    return result
+    
