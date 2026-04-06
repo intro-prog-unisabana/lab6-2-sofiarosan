af3fb75c8533972bb59b3c9a4796c7f08a033277
sofiarosan <anarodsan@unisabana.edu.co>
2026-04-06 18:58:53 -0500

    manager 1

---
+++ grades_manager.py
@@ -0,0 +1,54 @@
+def initialize_dict(student_name, subject_grades):
+    return {student_name: subject_grades}
+
+def add_student(student_grades=None):
+    if student_grades is None:
+        student_grades={}
+
+    name = input("Enter student name:\n").strip().title()
+
+    subjects = {}
+
+    while True:
+
+        entrada= input ("Enter subject and grade (or 'exit' to finish):\n").strip()
+        if entrada.lower() == exit:
+            break
+
+        subject, grade= entrada.split(",")
+        subject = subject.strip().title()
+        grade=float(grade.strip())
+
+
+        subjects[subject]=grade
+    
+    student_grades[name]= subjects
+
+    print(f"Student {name} successfully added to the grades management system.\n")
+
+    return student_grades
+
+def get_students(student_grades, keys):
+
+    result={}
+
+    for name in keys:
+        name_title = name.title()
+
+        if name_title in student_grades:
+            result[name_title]= student_grades[name_title]
+        else:
+            print(f"{name_title} not found!")
+
+    return result
+
+def avg_by_student(student_grades, keys=None):
+
+    if keys is None:
+        selected= student_grades
+    else:
+        selected= get_students(student_grades, keys)
+
+    for student, grades in selected.items():
+        avg = sum(grades.values())/len(grades)
+        print(f"{student}: {avg:.1f}")
