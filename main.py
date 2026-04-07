bb6411a9a1f26e569f621ce9eacc7e7b499b9474
sofiarosan <anarodsan@unisabana.edu.co>
2026-04-06 21:18:11 -0500

    main 2

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     my_grades = {}
 
     while True:
-        print("\nSelect an option:")
+        print("Select an option:")
         print("1. Add a student")
         print("2. Print student grade averages")
         print("3. Exit")
@@ -16,17 +16,17 @@ def main():
         if choice == "1":
             my_grades= add_student(my_grades)
         elif choice == "2":
-            print("\nSelect an option:")
+            print("Select an option:")
             print("a. Display all students")
             print("b. Display selected students")
 
-            sub_choice = input.strip()
+            sub_choice = input().strip()
 
             if sub_choice == "a":
                 avg_by_student(my_grades)
             elif sub_choice == "b":
                 print("Enter student names (comma-separated):")
-                names = input.split(",")
+                names = input().split(",")
 
                 names = [name.strip() for name in names]
 
